951ad840a2c4043eccd990e20af4c41452999369
Caden Wright <119685561+05cwright05@users.noreply.github.com>
2026-04-06 19:00:16 -0500

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # Alright so if we dont have an open parenthesis we CANNOT put a clsoe parentehseis
+        # Otherwise we have either as a choice
+        # So we should maintain a count: num_open
+        # If there is 1 open and 1 clsoe that should be said to be 0
+        string = []
+        output = []
+        def backtrack(string, num_open, open_left, close_left):
+            if open_left == 0 and close_left == 0:
+                output.append("".join(string))
+                return
+            if open_left > 0:
+                num_open+=1
+                open_left-=1
+                string.append("(")
+                backtrack(string, num_open, open_left, close_left)
+                num_open-=1
+                open_left+=1
+                string.pop()
+            if num_open > 0:
+                num_open-=1
+                close_left-=1
+                string.append(")")
+                backtrack(string, num_open, open_left, close_left)
+                string.pop()
+
+        backtrack([], 0, n, n)
+
+        return output
