9e706437fbdf6a19f48e2b55c2f919e1873a4969
Caden Wright <119685561+05cwright05@users.noreply.github.com>
2026-04-08 11:06:16 -0500

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # we are given an array of heights
+        # these heights represent the height of a bar that I could choose to place there
+        # to maximize height we need to maximize distance apart and height
+
+        # gotta ne some kind of 2 pointer / sliding window problem
+
+        # essentially i start with left pointer at index 0 and right pinter at index 1, then i move my right pointer froward and recrod the total volume
+
+        # naive approach if for each left wall try every possible right wall i.e. n^2
+
+        # waht if we knew the best capacity we could make at each spot like a suffix sum array with respect to the beginning
+        # key observation: we will never choose a lower bar for our left ;ointer
+        # that is we can do a lot of skipping
+        # so for the first one we choose the very first height, but after taht we jumkp the left point to the right pointer
+        # if right is > left. Becuase imagine that if you are moving along i wouold never start more right at a lower point
+        # i may start more right only if it increases the height
+
+        # the worst case here is strictly descending heights - but do you think it would be appropriate to code up what we have right now
+        # or should i keep thinking on it
+
+        left = 0
+        right = 1
+        highest_left = 0
+        best_volume = 0
+        while left < len(heights)-1:
+            if heights[left] <= highest_left:
+                left+=1
+                right=left+1
+                continue
+            highest_left = heights[left]
+            
+            while right < len(heights):
+                # compute the volume
+                best_volume = max(best_volume, (right - left) * min(heights[left], heights[right]))
+                right+=1
+        return best_volume
