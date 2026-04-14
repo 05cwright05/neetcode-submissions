49a1e55eb97c08c3ae25664f4df2a07293bdc5b8
Caden Wright <119685561+05cwright05@users.noreply.github.com>
2026-04-13 21:34:37 -0500

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        #first thought is that we go thru the tree and for every node we see
+        # we add it to a min-heap this approach would be NlogN + k 
+
+        # wondering if we can do in a single pass give =n that it is indeed a binary serach tree,
+        # surely there is a way to exploit this
+        # definitely gotta be a way.
+        # like we recurse down using a postorder traversal and then we recurse back up the stack n-1 times 
+        # like if i visit all nodes ysing an in order traversal and then add them as a list as i do that then the k-1 element will be what im looking for
+        nodes = []
+        cnt = k
+        res = root
+        def dfs(curr):
+            nonlocal cnt, res
+            if curr == None:
+                return 
+            dfs(curr.left)
+            if cnt == 0:
+                return
+            cnt-=1
+            if cnt == 0:
+                res = curr.val
+                return
+            dfs(curr.right)
+        dfs(root)
+        return res
+
+
+
